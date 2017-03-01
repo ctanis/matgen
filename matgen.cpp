e652155ad2c2d59afdf06dbfa9ad9da0a47299a5
Craig Tanis <craig-tanis@utc.edu>
2017-03-01 12:22:47 -0500

    somewhat faster

--- matgen.cpp
+++ matgen.cpp
@@ -5,6 +5,15 @@
 #include <time.h>
 
 
+#define MYRAND() rand()
+
+// int g_seed;
+// inline int fastrand() { 
+//   g_seed = (214013*g_seed+2531011); 
+//   return (g_seed>>16)&0x7FFF; 
+// } 
+
+
 
 int main(int argc, char *argv[])
 {
@@ -13,7 +22,7 @@ int main(int argc, char *argv[])
     double var = atof(argv[3]);
 
     srand(time(NULL) ^ getpid());
-
+    //g_seed=time(NULL) ^ getpid();
     
     std::vector<int>    ia;
     std::vector<int>    ja;
@@ -25,7 +34,7 @@ int main(int argc, char *argv[])
     fprintf(stderr, "reserving %d\n", res_size);
 
     ja.reserve(res_size);
-    data.reserve(ja.size());
+    data.reserve(res_size);
 
     for (int i=0; i<dim; i++)
     {
@@ -36,25 +45,27 @@ int main(int argc, char *argv[])
 
         ia[i] = ja.size()+1;
 
-        // count of items in this row
-        //double rate = var*(1.0*rand()/RAND_MAX-0.5)+density;
+        // calculate a likelihood that a given column has a non-zero, so that
+        // we get near (1 +/- var)*nnz_row
+        // double chance = 0.5(1.0+var*rand()/RAND_MAX)*nnz
 
-        double rate = 1.0*nnz_row / dim+var*nnz_row*(1.0*rand()/RAND_MAX-0.5)/dim;
+        double chance = (1 - var + 2.0*var*MYRAND()/RAND_MAX)*nnz_row / dim;
 
+        if (chance < 0) chance = 0;
+        if (chance > 1) chance = 1;
 
-        if (rate < 0) rate = 0;
-        if (rate > 1) rate = 1;
-
-        int thresh = (int)(RAND_MAX*rate);
+        int thresh = (int)(RAND_MAX*chance);
         // count * random columns
 
 
         for (int j=0; j<dim; j++)
         {
-            if (rand() < thresh)
+            if (j == i || MYRAND() < thresh) // force a diagonal
             {
                 ja.push_back(j);
-                data.push_back(1000.0*rand()/RAND_MAX-50.0);
+
+                // weird scale factor
+                data.push_back(1000.0*MYRAND()/RAND_MAX-500.0);
             }
         }
     }
